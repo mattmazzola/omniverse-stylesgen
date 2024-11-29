cf84b78b833984017437e28f24da12c5c68c5f21
Matt Mazzola <mattmazzola@live.com>
2024-11-29 13:11:45 -0800

    reorganize main

--- plugin-src/code.ts
+++ plugin-src/code.ts
@@ -1,9 +1,7 @@
-import { Language } from "./types"
 import { onPreferencesChanged, onGenerate } from "./callbacks"
 
 if (figma.mode === "codegen") {
   figma.codegen.on("preferenceschange", onPreferencesChanged)
-  figma.showUI(__html__, { visible: false })
-
   figma.codegen.on("generate", onGenerate)
+  figma.showUI(__html__, { visible: false })
 }
