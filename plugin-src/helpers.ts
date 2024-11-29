073ee6a348c5dbd88870b7eb2abffb3c224775ab
Matt Mazzola <mattmazzola@live.com>
2024-11-29 13:40:14 -0800

    restructure helpers

--- plugin-src/helpers.ts
+++ plugin-src/helpers.ts
@@ -1,40 +1,95 @@
 import { rgbToHex } from "./utilities"
 
-export async function processCollection({ name, modes, variableIds }: { name: string, modes: any[], variableIds: string[] }) {
-    const files = []
-    for (const mode of modes) {
-      const file = { fileName: `${name}.${mode.name}.tokens.json`, body: {} }
-      for (const variableId of variableIds) {
-        const variable: Variable = await figma.variables.getVariableByIdAsync(variableId)
-        const { name, resolvedType, valuesByMode } = variable
-  
-        const value = valuesByMode[mode.modeId]
-        if (value !== undefined && ["COLOR", "FLOAT"].includes(resolvedType)) {
-          let obj: any = file.body
-          
-          name.split("/").forEach((groupName) => {
-            obj[groupName] = obj[groupName] || {}
-            obj = obj[groupName]
-          })
-          
-          obj.$type = resolvedType === "COLOR" ? "color" : "number"
-          if (value.type === "VARIABLE_ALIAS") {
-            const currentVar: Variable = await figma.variables.getVariableByIdAsync(
-              value.id
-            )
-            obj.$value = `{${currentVar.name.replace(/\//g, ".")}}`
-          } else {
-            if (resolvedType === "COLOR") {
-                const rgba: RGBA = value
-                obj.$value = rgbToHex(rgba)
-            } else {
-                obj.$value = value
-            }
-          }
-        }
-      }
-      files.push(file)
-    }
-    return files
+export async function processCollection(collection: VariableCollection) {
+  const files = []
+
+  if (collection.name.toLowerCase().includes("primitives")) {
+    const primativeFiles = await getPrimativesFiles(collection)
+    files.push(...primativeFiles)
   }
+
+  return files
+}
+
+async function getPrimativesFiles(collection: VariableCollection) {
+  const { variableIds, modes } = collection
   
+  // // TODO: Add support for multiple modes
+  const mode = modes[0]
+  
+  const colorsFile = { fileName: "colors.ts", body: {} }
+  const fontsFile = { fileName: "fonts.ts", body: {} }
+  const spacingFile = { fileName: "spacing.ts", body: {} }
+  
+  for (const variableId of variableIds) {
+    const variable = await figma.variables.getVariableByIdAsync(variableId)
+    if (!variable) {
+      throw new Error(`Variable with ID ${variableId} not found`)
+    }
+    
+    if (variable.name.toLowerCase().includes("color")) {
+      const processedColorVariable = await processColorVariable(variable, mode)
+      Object.assign(colorsFile.body, processedColorVariable)
+    }
+  }
+
+  const files = [
+    colorsFile,
+    fontsFile,
+    spacingFile,
+  ]
+
+  return files
+}
+
+
+async function getTokensFile(collection: VariableCollection) {
+  return getPrimativesFiles(collection)
+}
+
+function getRootFile(collection: VariableCollection) {
+  const file = { fileName: "__init__.ts", body: {} }
+
+  return file
+}
+
+type VariableMode = {
+  modeId: string
+  name: string
+}
+
+async function processColorVariable(variable: Variable, mode: VariableMode) {
+  const { name, resolvedType, valuesByMode } = variable
+  const rootObj: any = {}
+  let obj = rootObj
+
+  const value = valuesByMode[mode.modeId]
+  if (value !== undefined && ["COLOR", "FLOAT"].includes(resolvedType)) {
+
+    name.split("/").forEach((groupName) => {
+      obj[groupName] = obj[groupName] || {}
+      obj = obj[groupName]
+    })
+
+    obj.$type = resolvedType === "COLOR" ? "color" : "number"
+
+    const variableAlias = value as VariableAlias
+    if (variableAlias.type === "VARIABLE_ALIAS") {
+      const currentVar = await figma.variables.getVariableByIdAsync(variableAlias.id)
+      if (!currentVar) {
+        throw new Error(`Variable with ID ${variableAlias.id} not found`)
+      }
+      obj.$value = `{${currentVar.name.replace(/\//g, ".")}}`
+    } else {
+      if (resolvedType === "COLOR") {
+        // Assume COLOR is always RGBA ?
+        const rgba = value as RGBA
+        obj.$value = rgbToHex(rgba)
+      } else {
+        obj.$value = value
+      }
+    }
+  }
+
+  return rootObj
+}
