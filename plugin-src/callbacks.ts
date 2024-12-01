75640018a5e47a34e4618b4ac72aca14bd2cbc28
Matt Mazzola <mattmazzola@live.com>
2024-12-01 15:21:26 -0800

    Remove on generate code format

--- plugin-src/callbacks.ts
+++ plugin-src/callbacks.ts
@@ -31,64 +31,8 @@ export async function onPreferencesChanged(event: CodegenPreferencesEvent) {
 
         figma.ui.postMessage(message)
     }
-
 }
 
 export async function onGenerate(event: CodegenEvent): Promise<CodegenResult[]> {
-    const { node, language } = event
-    const nodeObject = {
-        type: node.type,
-        name: node.name,
-        width: node.width,
-        height: node.height,
-    }
-
-    const blocks: CodegenResult[] = []
-
-    if (language.toLowerCase() === Language.JSON) {
-        const block2: CodegenResult = {
-            title: `Custom JSON`,
-            code: JSON.stringify(nodeObject),
-            language: "JSON",
-        }
-        blocks.push(block2)
-    }
-
-    // For each block, add FORMAT, ID, and post it to the UI to be formatted
-    blocks.forEach(({ language, code }, index) => {
-        const message = {
-            event: MessageEvents.FORMAT,
-            id: index,
-            code,
-            language,
-        }
-
-        figma.ui.postMessage(message)
-    })
-
-    let expectedMessageCount = blocks.length
-    const formattedBlocks: any[] = []
-
-    let promiseResolve: (value: CodegenResult[]) => void
-    const promise = new Promise<CodegenResult[]>((resolve) => {
-        promiseResolve = resolve
-    })
-
-    figma.ui.onmessage = (message) => {
-        if (message.event === MessageEvents.FORMAT_RESULT) {
-            // Create block using with original block with the formatted code
-            const originalBlock = blocks[message.id]
-            formattedBlocks[message.id] = Object.assign(originalBlock, { code: message.result, })
-
-            // Decrement the expected message count
-            expectedMessageCount -= 1
-        
-            // If no remaining messages expected, then resolve the promise with the results
-            if (expectedMessageCount <= 0) {
-                promiseResolve(formattedBlocks)
-            }
-        }
-    }
-
-    return promise
+    return []
 }
--- ui-src/app.js
+++ ui-src/app.js
@@ -1,63 +1,11 @@
-import prettier from "prettier/esm/standalone.mjs"
-import parserBabel from "prettier/esm/parser-babel.mjs"
-import parserHTML from "prettier/esm/parser-html.mjs"
-import parserCSS from "prettier/esm/parser-postcss.mjs"
 import JSZip from "jszip"
 
 const MessageEvents = {
-  FORMAT: "FORMAT",
-  FORMAT_RESULT: "FORMAT_RESULT",
   PREFERENCES_CHANGED: "PREFERENCES_CHANGED",
 }
 
-const PRINT_WIDTH = 50
-
-function formatCode({ language, code, printWidth = PRINT_WIDTH }) {
-  switch (language) {
-    case "HTML":
-      return prettier.format(code, {
-        printWidth,
-        parser: "html",
-        plugins: [parserHTML],
-        htmlWhitespaceSensitivity: "ignore",
-        bracketSameLine: false,
-      })
-    case "CSS":
-      return prettier.format(code, {
-        printWidth,
-        parser: "css",
-        plugins: [parserCSS],
-      })
-    case "JSON":
-      return JSON.stringify(JSON.parse(code), null, 2)
-    case "JAVASCRIPT":
-    case "TYPESCRIPT":
-      return prettier.format(code, {
-        printWidth,
-        parser: "babel-ts",
-        plugins: [parserBabel],
-        semi: true,
-      })
-  }
-}
-
-function onFormatMessage(pluginMessage) {
-  const result = formatCode(pluginMessage)
-  parent.postMessage(
-    {
-      pluginMessage: {
-        event: MessageEvents.FORMAT_RESULT,
-        id: pluginMessage.id,
-        result,
-      },
-    },
-    "*"
-  )
-}
 
 async function onPreferencesChangeMessage(pluginMessage) {
-  const downloadButton = document.getElementById("download")
-  console.log({ downloadButton })
 
   const zip = new JSZip()
   pluginMessage.files.forEach(file => {
